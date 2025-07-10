58078be4f7c5cb09606b45696ff05e5a6840bf6e
Alejandro Mederico <mederico.alejandro1988@gmail.com>
2025-07-10 10:44:14 -0300

    acomodamos wrapper de usuario

--- modulo4_agente_empleo/frontend/src/pages/dashboard/UserDashboardWrapper.tsx
+++ modulo4_agente_empleo/frontend/src/pages/dashboard/UserDashboardWrapper.tsx
@@ -1,9 +1,16 @@
 import Dashboard from './Dashboard';
+import JobSearchConfigPage from './JobSearchConfigPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-const UserDashboardWrapper = () => (
-  <>
-    <Dashboard />
-  </>
-);
+
+const UserDashboardWrapper = () => {
+  return (
+    <Routes>
+      <Route path="" element={<Dashboard />} />
+      <Route path="configuracion-busqueda" element={<JobSearchConfigPage />} />
+      <Route path="*" element={<Navigate to="" replace />} />
+    </Routes>
+  );
+};
 
 export default UserDashboardWrapper;
